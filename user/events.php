aa4c6a7b8563ca7a54547ec3f16ba53658fcf3a6
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-12-07 15:45:49 +0100

    Update events.php

--- user/events.php
+++ user/events.php
@@ -53,6 +53,7 @@ $events = $stmt->fetchAll();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/main.min.css" rel="stylesheet">
+<script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
     <style>
         body {
             background-color: #f8f9fa;
