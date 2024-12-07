44d216aee13e202c33dca2aac19e7cf43e10b726
eloisarrazin84 <eloi.sarrazin@outdoorsecours.fr>
2024-12-07 15:53:49 +0100

    Update events.php

--- user/events.php
+++ user/events.php
@@ -52,8 +52,8 @@ $events = $stmt->fetchAll();
     <title>Événements</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/main.min.css" rel="stylesheet">
-<script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
+   <link rel="stylesheet" href="https://uicdn.toast.com/calendar/latest/toastui-calendar.min.css" />
+<script src="https://uicdn.toast.com/calendar/latest/toastui-calendar.min.js"></script>
     <style>
         body {
             background-color: #f8f9fa;
